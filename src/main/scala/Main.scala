3ccbda5f513742d94b00aee8cddfdd5915d9d0bd
Guillaume Marceau <guillaume.marceau@sefaira.com>
2013-08-11 19:37:35 -0400

    algorithm written down

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,45 +1,112 @@
-import collection.mutable.{Map => MMap, ListBuffer}
-import org.supercsv.prefs.CsvPreference
-import scala.io.Source
-import java.io.{File, FileReader}
-import org.supercsv.io._
-import cc.spray.json._
-import JsonTransforms._
-import scala.collection.JavaConversions._
+import java.io.File
 import java.util.Date
+import scala.math._
+import DataConversion._
 
+case class Item(date: Date, category: List[String], payee: String, memo: String, outflow: Double, inflow: Double) {
+  def label: String = {
+    val skipList = List("DBT CRD", "POS DEB", "PPD", "Payroll", "Paypal", "PAYPAL", "Payee",
+                        "T MOBILE", "ATM FEE", "AMAZON", "Amazon", "Check ", "CREDIT", "DDA", "DEBIT", "e-Pay", "Interest Deposit",
+                        "KEY FOODS", "FOODTOWN", "Adjustment", "MTA", "Transfer", "USPS").map(_.r)
+    def skip(str: String): Boolean = skipList.find(_.findFirstIn(str).isDefined).isDefined
+    List(memo, payee, category.last).find(!skip(_)).get
+  }
+  def amt = outflow - inflow
 
-case class Item(date: Date, category: List[String], payee: String, memo: String, outflow: Double, inflow: Double)
+}
+case class Category(name: String, items: List[Item]) {
+  def expenses = items.filter(_.amt > 0).map(_.amt).sum
+  def income = -items.filter(_.amt < 0).map(_.amt).sum
+  def absorbIncomeIntoExpenses = {
+    val ratio = (expenses - income) / expenses
+    val result = for (i <- items) yield { i.copy(outflow = i.outflow * ratio, inflow = 0) }
+    this.copy(items = result)
+  }
+}
+
+object Block {
+  def apply(area: Double, item: Item) = Block(List(), 0, area, item)
+}
+case class Block private(private val _top: List[Block], spacer: Int, area: Double, item: Item) {
+  def top = _top.reverse
+  val width: Int = max(1, _top.map(_.width).sum + spacer)
+  val height: Double = area / width
+  val ratio: Double = height / width
+  val weightedDate: Date = {
+    val sumArea = _top.map(_.area).sum + area
+    val here = item.date * area / sumArea
+    val up = (for(b <- _top) yield b.weightedDate * b.area / sumArea).sum
+    (here + up).toLong
+  }
+
+  def add(other: Block) = this.copy(_top = other :: _top)
+  def widen = this.copy(spacer=spacer+1)
+}
 
 object Main {
-  import Protocol._
-  implicit val itemFormat = jsonFormat6(Item)
+  val millisecondsPerDay = 1000d * 60 * 60 * 24
+  private val targetRatio = 1 / 1.61803398875
 
-  def asImmutable[A, B](m: MMap[A, B]) = Map(m.toList : _*)
-  def isTransfer(i: Item) = i.category == List()
-  def read(f: File) = {
-    val reader = new CsvMapReader(new FileReader(f), CsvPreference.STANDARD_PREFERENCE)
-    val headers = reader.getHeader(true)
-    val maps = Stream.continually(reader.read(headers:_*)).takeWhile(_ != null).toList
-    val jsons =
-      for(m <- maps) yield
-        asImmutable(m).toJson.rename("Date", "date")
-                             .rename("Category", "category")
-                             .transform("category", (str:String) => if (str==null) Array[String]() else str.split(":"))
-                             .rename("Memo", "memo").transform("memo", (str:String) => if (str==null) "" else str)
-                             .rename("Payee", "payee").transform("payee", (str:String) => if (str==null) "" else str)
-                             .rename("Outflow", "outflow").transform("outflow", (str:String) => str.replace("$", "").toDouble)
-                             .rename("Inflow", "inflow").transform("inflow", (str:String) => str.replace("$", "").toDouble)
+  object fitnessWeights {
+    val targetRatio = 1d
+    val dateAlignment = 1d * (1 / millisecondsPerDay)
+  }
 
-    jsons.map(_.convertTo[Item]).filterNot(isTransfer(_)).sortBy(_.date)
+
+  def zipRows(widthPerDay: Double, here: List[Block], above: List[Block]): List[Block] = {
+
+    def score(b: Block, nextDate: Option[Date]): Double = {
+
+      val x = fitnessWeights.targetRatio * abs(b.ratio - targetRatio)
+      val y = nextDate match { case Some(d) =>
+                                  val blockRightEdgeDate = b.weightedDate + b.width / widthPerDay
+                                  fitnessWeights.dateAlignment * max(0, blockRightEdgeDate - d)
+                               case None => 0
+                             }
+      x + y
+    }
+
+    def optimizeOnce(b: Block, above: List[Block], nextDate: Option[Date]): (Block, List[Block]) = {
+      def option(candidate: Block) = Some(score(candidate, nextDate) -> candidate)
+      val optionNoop = option(b)
+      val optionWiden = option(b.widen)
+      val optionAdd = if (above.isEmpty) None else option(b.add(above.head))
+
+      val choice = List(optionNoop, optionWiden, optionAdd).minBy(_.map(_._1)).get
+      if (choice eq optionAdd)
+        (choice._2, above.tail)
+      else (choice._2, above)
+
+    }
+
+    def optimize(b: Block, above: List[Block], nextDate: Option[Date]): (Block, List[Block]) = {
+      val (newB, newOther) = optimizeOnce(b, above, nextDate)
+      if (b eq newB) (newB, newOther)
+      else optimize(newB, newOther, nextDate)
+    }
+
+    def loop(here: List[Block], above: List[Block]): List[Block] = here match {
+      case Nil => List()
+      case here1 :: Nil =>
+        List(above.foldLeft(here1) { _.add(_) })
+      case here1 :: here2 :: tail =>
+        val (b, restAbove) = optimize(here1, above, Some(here2.item.date))
+        b :: loop(here, restAbove)
+    }
+
+    loop(here, above)
   }
 
   def main(args: Array[String]) {
     val homeDir = System.getenv("HOME")
     val dir = new File(homeDir, "Dropbox/YNAB/Exports")
     val files = dir.listFiles().filter(_.getName.matches(".*-Register\\.csv")).sortBy(_.lastModified())
-    val data = read(files.last)
-    data.foreach(println)
+    val (expenses, incomes) = Protocol.read(files.last)
+                                       .map(_.absorbIncomeIntoExpenses)
+                                       .partition { c => c.expenses > c.income }
+    expenses.foreach(println)
+    println()
+    incomes.foreach(println)
   }
 
 }
--- src/main/scala/Protocol.scala
+++ src/main/scala/Protocol.scala
@@ -1,6 +1,13 @@
 import cc.spray.json._
+import collection.mutable.{ Map => MMap }
+import java.io.{FileReader, File}
 import java.text.SimpleDateFormat
 import java.util.Date
+import org.supercsv.io.CsvMapReader
+import org.supercsv.prefs.CsvPreference
+import cc.spray.json._
+import JsonTransforms._
+import scala.collection.JavaConversions._
 
 object Protocol extends cc.spray.json.DefaultJsonProtocol {
   implicit val dateFormat = new JsonFormat[Date] {
@@ -10,4 +17,29 @@ object Protocol extends cc.spray.json.DefaultJsonProtocol {
       case JsString(str) => new SimpleDateFormat(dateFormatString).parse(str)
       case _ => throw new DeserializationException("Cannot turn JsValue into a Date: " + value)
   } }
+
+  implicit val itemFormat = jsonFormat6(Item)
+
+  def asImmutable[A, B](m: MMap[A, B]) = Map(m.toList : _*)
+  def isTransfer(i: Item) = i.category == List()
+  def read(f: File): List[Category] = {
+    val reader = new CsvMapReader(new FileReader(f), CsvPreference.STANDARD_PREFERENCE)
+    val headers = reader.getHeader(true)
+    val maps = Stream.continually(reader.read(headers:_*)).takeWhile(_ != null).toList
+    val jsons =
+      for(m <- maps) yield
+        asImmutable(m).toJson.rename("Date", "date")
+          .rename("Category", "category")
+          .transform("category", (str:String) => if (str==null) Array[String]() else str.split(":"))
+          .rename("Memo", "memo").transform("memo", (str:String) => if (str==null) "" else str)
+          .rename("Payee", "payee").transform("payee", (str:String) => if (str==null) "" else str)
+          .rename("Outflow", "outflow").transform("outflow", (str:String) => str.replace("$", "").toDouble)
+          .rename("Inflow", "inflow").transform("inflow", (str:String) => str.replace("$", "").toDouble)
+
+    val items = jsons.map(_.convertTo[Item]).filterNot(isTransfer(_)).sortBy(_.date)
+    val categories =
+      for((cat, lst) <- items.groupBy(_.category.head).toList) yield Category(cat, lst)
+    categories.sortBy(- _.items.size)
+  }
+
 }
