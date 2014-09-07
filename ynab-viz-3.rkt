5ff4a63417c8ba1ce5faca2f4cf7989448a346f5
Guillaume Marceau <guillaume.marceau@sefaira.com>
2014-09-06 20:23:57 -0400

    draft viz from inkscape is now implemented in rkt

---
+++ ynab-viz-3.rkt
@@ -0,0 +1,145 @@
+#lang racket
+(require slideshow/pict
+         gmarceau/all
+         racket/draw
+         )
+
+(define dirname "C:/Documents/Dropbox/YNAB")
+
+(define filename "Linnea and Guillaume-Report-Income v. Expense Aug '13 to Aug '14.processed.csv")
+
+(define lines 
+  (map (lambda (line) (string-split line ","))
+       (file->lines (build-path dirname filename))))
+
+(define categories (map first (rest lines)))
+
+(define month-names (rest (first lines)))
+(define incomes (for/list ([i (rest (second lines))]) (string->number i)))
+
+(define (parse-months lines)
+  (for/list ([column (transpose (map rest (rest lines)))]
+                          [income incomes]
+                          [n month-names])
+                 (hash 'name n 
+                       'income income 
+                       'expenses (list->hash (rest (map list categories (map (lambda-pipe (string->number <>) (- <>)) column)))))))
+  
+(define (absorb-refunds m)
+  (define sum (apply + (filter negative? (hash-values (.. m 'expenses)))))
+  (pipe m 
+        (!! <> 'income (+ (.. m 'income) sum))
+        (!! <> 'expenses (hash-map-values:h (.. m 'expenses) (// max <> 0)))))
+  
+(define months (map absorb-refunds (parse-months lines)))
+
+(define averages 
+  (list->hash
+   (for/list ([c (rest categories)])
+     (list c (apply average (for/list ([m months])
+                              (.. m 'expenses c)))))))
+  
+(define sorted-averages (sort (hash->list averages) < #:key second))
+
+
+(define (group n lst)
+
+  (define (recur target left lst)
+    (match lst
+      [(list (list cat _)) (list (list cat))]
+      [(cons (and hd (list cat amt)) tail)
+       (cond [(<= left amt)
+              (define next (recur target target tail))
+              (cons (list cat) next)]
+             [else 
+              (define next (recur target (- left amt) tail))
+              (cons (cons cat (first next)) (rest next))])]))
+  
+  (define s (/ (apply + (hash-values averages)) n))
+  (define result (recur s s (reverse sorted-averages)))
+  (reverse result))
+  
+  
+  
+(define grouped-categories (group 3 sorted-averages))
+        
+(define ratio 2.3)
+(define average-income (apply average (column months 'income)))
+(define height (sqrt (* average-income ratio)))
+(define width (/ average-income height))
+
+(define colors
+  (hash "savings" (make-object color% 229 255 213)
+        "Communication" (make-object color% 0 226 255)
+        "Stuff" (make-object color% 0 198 255)
+        "Generosity" (make-object color% 0 169 255)
+        "Health" (make-object color% 0 141 255)
+        "Transport" (make-object color% 0 127 255)
+        "Joy" (make-object color% 0 98 255)
+        "Food" (make-object color% 98 255 155)
+        "The Man" (make-object color% 198 255 56)
+        "House & Upkeep" (make-object color% 255 56 0)
+        "Momentous" (make-object color% 255 102 0)
+        ))
+
+(define (draw-cell h a c)
+  (if (or (= h 0) (= a 0))
+      (blank)
+      (let ()
+        (define w (/ a h))
+        (cc-superimpose
+         (colorize (filled-rectangle (/ a h) h) c)
+         (cellophane (rectangle w h) 0.1)))))
+
+
+(define (draw-row amounts colors)
+  (define h (/ (apply + amounts) width))
+  (apply hb-append (map (// draw-cell h <> <>) amounts colors)))
+
+(define (draw-stack groups colors)
+  (apply vl-append (map draw-row groups colors)))
+
+(define (draw-month month)
+  (define s
+    (draw-stack
+     (for/list ([cats grouped-categories])
+       (map (// .. month 'expenses <>) cats))
+     (mapmap  (// .. colors <>) grouped-categories)))
+  
+  (vc-append
+   (inset (text (.. month 'name)) 0 (* width 1/3))
+   (if (positive? (month-savings month))
+       (vl-append 
+        (draw-row (list (month-savings month)) (list (.. colors "savings")))
+        s)
+      s)))
+   
+(define (month-expenses m)
+  (apply + (hash-values (.. m 'expenses))))
+(define (month-savings m)
+  (- (.. m 'income) (month-expenses m)))
+(define (month-savings-rate m)
+  (/ (month-savings m) (month-expenses m)))
+
+(define (draw-months months)
+  (define mx-burn (- (min 0 (apply min (map month-savings months)))))
+
+  (define stacks
+    (apply hb-append (/ width 2)
+           (for/list ([m months])
+             (define delta (+ mx-burn (min 0 (month-savings m))))
+             (vl-append (draw-month m)
+                        (colorize (blank width (/ delta width)) "red")))))
+  (define zero-line (cellophane (linewidth 2 (colorize (hline (+ (pict-width stacks) (* width 2/3)) 1) "darkgray")) 0.4))
+  (panorama (pin-over stacks (* width -1/3) (- (pict-height stacks) (/ mx-burn width) 1) zero-line)))
+  
+(draw-months months)
+
+
+
+
+
+
+
+
+
